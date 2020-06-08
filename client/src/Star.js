b38f5e092b3903b7cc13799cd5d15b89b5c876df
Shafiulazam33 <mdshafiulazamhany@gmail.com>
2020-06-09 00:14:54 +0600

    last99

--- client/src/Star.js
+++
@@ -1,6 +0,0 @@
-let Star=(props)=> {
-  return (
-    <li>&#11088</li>
-  );
-}
-export default Star;
--- client/src/components/Faq.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Faq() {
-    return (
-        <div>
-            Any question it is faq
-        </div>
-    )
-}
