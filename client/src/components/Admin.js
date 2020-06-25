a2495885d42d28ecb589696c4131eb202fd8cc68
Shafiulazam33 <57700867+Shafiulazam33@users.noreply.github.com>
2020-06-25 22:52:15 +0600

    Update Admin.js

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -11,7 +11,7 @@ const Admin = () => {
     const [dataPerPage, setDataPerPage] = useState(10);
     const [displayNone, setDisplayNone] = useState("");
     const funcFindFeaturedPost = () => {
-        Axios.get('/api/job/find-featured-post')
+        Axios.post('/api/job/find-featured-post')
             .then((res) => {
                 setData(res.data.jobs)
                 setDataLength(res.data.jobs.length)
